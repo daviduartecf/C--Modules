ef854e928b89655809eba320c798ea12c574f69f
daduartehome <daviduartecf@gmail.com>
2025-01-28 19:31:30 +0000

    minor changes and comments

--- C02_Module/ex01/Fixed.cpp
+++ C02_Module/ex01/Fixed.cpp
@@ -8,6 +8,22 @@ std::ostream&   operator<<(std::ostream& os, const Fixed& fixed)
     return os;
 }
 
+//To store a int as a fixed-point just scale up 2^fractionalbits 
+//shift left
+Fixed::Fixed(int const num) {
+    std::cout << "Int constructor called" << std::endl;
+    _value = num << _fractionalBits;
+}
+
+//To store a float as a fixed-point we cant bitshift because 
+//floating-point nums are not integers
+//BIT SHIFT only work on integers
+//So we multiply by 2^fractionalbits
+Fixed::Fixed(float const num) {
+    std::cout << "Float constructor called" << std::endl;
+    _value = roundf(num * (1 << _fractionalBits));
+}
+
 float   Fixed::toFloat(void) const {
     return (float)(_value / (float)(1 << _fractionalBits));
 }
@@ -15,17 +31,6 @@ float   Fixed::toFloat(void) const {
 int     Fixed::toInt(void) const {
     return (_value >> _fractionalBits);
 }
-
-Fixed::Fixed(int const num) {
-    std::cout << "Int constructor called" << std::endl;
-    _value = num << _fractionalBits;
-}
-
-Fixed::Fixed(float const num) {
-    std::cout << "Float constructor called" << std::endl;
-    _value = roundf(num * (1 << _fractionalBits));
-}
-
 Fixed::Fixed(): _value(0) {
     std::cout << "Default constructor called" << std::endl;
 }
