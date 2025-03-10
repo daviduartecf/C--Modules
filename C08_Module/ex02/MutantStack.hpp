dee72909794690fe03bda05f0c77194b4444dbab
David Duarte E Costa Fernandes <daduarte@c2r4s1.42porto.com>
2025-03-10 16:39:43 +0000

    working on cpp08

--- C08_Module/ex02/MutantStack.hpp
+++ C08_Module/ex02/MutantStack.hpp
@@ -2,6 +2,8 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <iostream>
+#include <iterator>
 
 template <typename T>
 class MutantStack: public std::stack<T> {
@@ -10,6 +12,21 @@ class MutantStack: public std::stack<T> {
 		~MutantStack();
 		MutantStack& operator = (const MutantStack& other);
 		MutantStack(const MutantStack& other);
+		typedef typename MutantStack<T>::container_type::iterator iterator;
+		typedef typename MutantStack<T>::container_type::const_iterator const_iterator;
+		typedef typename MutantStack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		typedef typename MutantStack<T>::container_type::reverse_iterator reverse_iterator;
+
+		iterator begin();
+		iterator end();
+		const_iterator begin() const;
+		const_iterator end() const;
+		reverse_iterator rbegin();
+		reverse_iterator rend();
+		const_reverse_iterator rand() const;
+		const_reverse_iterator rend() const;
 };
+
+#include "MutantStack.tpp"
 
 #endif
--- C08_Module/ex02/MutantStack.tpp
+++ C08_Module/ex02/MutantStack.tpp
@@ -12,7 +12,12 @@ MutantStack<T>::MutantStack(const MutantStack& other): std::stack<T>(other) {}
 template <typename T>
 MutantStack<T>& MutantStack<T>::operator = (const MutantStack& other) {
 	if (this != &other) {
-
+		std::stack<T>::operator=(other);
 	}
-	return *this
+	return *this;
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin() {
+	return (std::stack<T>::c.begin());
 }
